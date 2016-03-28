c8cf4ad3b3dee9583dda8bc57b0adb4d0a4f5e90
Timo Tuominen <tituomin@gmail.com>
2016-03-28 22:36:19 +0300

    Clear nio buffer iteration state before ops.

--- script/templates/arrays.py
+++ script/templates/arrays.py
@@ -29,6 +29,8 @@ t_write = partial(
 t_read_nio = partial(
     t_loop,
     body = """
+    directByteBufferValue.clear();
+    <% array_variable %>.clear();
     <% variable_in %> = <% array_variable %>.get<% type_name %>();
     if (<% variable_in %> == <% element_literal %>) {
         localPersistentValue = 1;
@@ -40,12 +42,16 @@ t_read_nio = partial(
 t_write_nio = partial(
     t_loop,
     body = """
+    directByteBufferValue.clear();
+    <% array_variable %>.clear();
     <% array_variable %>.put<% type_name %>(<% element_literal %>);
     """)
 
 t_read_nio_as_view = partial(
     t_loop,
     body = """
+    directByteBufferValue.clear();
+    <% array_variable %>.clear();
     <% variable_in %> = <% array_variable %>.get();
     if (<% variable_in %> == <% element_literal %>) {
         localPersistentValue = 1;
@@ -57,15 +63,20 @@ t_read_nio_as_view = partial(
 t_write_nio_as_view = partial(
     t_loop,
     body = """
+    <% array_variable %>.clear();
     <% array_variable %>.put(<% element_literal %>);
     """)
 
 t_bulk_read = """
 <% declare_variables %>
+directByteBufferValue.clear();
+<% array_variable %>.clear();
 <% array_variable %>.get(<% array_in %>, 0, current_size);
 """
 
 t_bulk_write = """
 <% declare_variables %>
+directByteBufferValue.clear();
+<% array_variable %>.clear();
 <% array_variable %>.put(<% array_in %>, 0, current_size);
 """
