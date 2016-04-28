816d5371a58b2c8c9233547d2bd9bd7e76183c65
Timo Tuominen <tituomin@gmail.com>
2016-04-28 20:59:22 +0300

    Use explicit index in all array benchmarks.

--- script/templates/arrays.py
+++ script/templates/arrays.py
@@ -3,11 +3,9 @@ from templating import partial
 t_loop = """
 <% declare_idx %>
 <% declare_variables %>
-idx = current_size;
-while (idx != 0) {
-    idx--;
+for (idx = 0; idx < current_size; idx++) {
     <% body %>
-} 
+}
 """
 
 t_read = partial(
@@ -29,9 +27,7 @@ t_write = partial(
 t_read_nio = partial(
     t_loop,
     body = """
-    directByteBufferValue.clear();
-    <% array_variable %>.clear();
-    <% variable_in %> = <% array_variable %>.get<% type_name %>();
+    <% variable_in %> = <% array_variable %>.get<% type_name %>(idx);
     if (<% variable_in %> == <% element_literal %>) {
         localPersistentValue = 1;
     }
@@ -42,17 +38,13 @@ t_read_nio = partial(
 t_write_nio = partial(
     t_loop,
     body = """
-    directByteBufferValue.clear();
-    <% array_variable %>.clear();
-    <% array_variable %>.put<% type_name %>(<% element_literal %>);
+    <% array_variable %>.put<% type_name %>(idx, <% element_literal %>);
     """)
 
 t_read_nio_as_view = partial(
     t_loop,
     body = """
-    directByteBufferValue.clear();
-    <% array_variable %>.clear();
-    <% variable_in %> = <% array_variable %>.get();
+    <% variable_in %> = <% array_variable %>.get(idx);
     if (<% variable_in %> == <% element_literal %>) {
         localPersistentValue = 1;
     }
@@ -63,20 +55,15 @@ t_read_nio_as_view = partial(
 t_write_nio_as_view = partial(
     t_loop,
     body = """
-    <% array_variable %>.clear();
-    <% array_variable %>.put(<% element_literal %>);
+    <% array_variable %>.put(idx, <% element_literal %>);
     """)
 
 t_bulk_read = """
 <% declare_variables %>
-directByteBufferValue.clear();
-<% array_variable %>.clear();
 <% array_variable %>.get(<% array_in %>, 0, current_size);
 """
 
 t_bulk_write = """
 <% declare_variables %>
-directByteBufferValue.clear();
-<% array_variable %>.clear();
 <% array_variable %>.put(<% array_in %>, 0, current_size);
 """
